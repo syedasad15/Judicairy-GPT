1e2f1b6ca992a442b36f5730a1902dbe87f7ee46
syedasad15 <144409006+syedasad15@users.noreply.github.com>
2025-08-05 11:21:43 +0500

    Update main.py

--- Implementation/main.py
+++ Implementation/main.py
@@ -7,12 +7,7 @@ import os
 from Agents import download_agent
 from utils import intent_classifier 
 from Agents.title_generator import generate_chat_title
-# ✅ Write the secret JSON to a temp file
-with open("gcloud_key.json", "w") as f:
-    f.write(st.secrets["google_cloud"]["credentials"])
 
-# ✅ Set the env variable to use the temp file
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcloud_key.json"
 st.set_page_config(page_title="PakLaw Judicial Assistant", layout="wide")
 
 # --- Session State Initialization ---
@@ -206,6 +201,7 @@ if submitted and (user_input or st.session_state.uploaded_case_text):
 
 
     st.rerun()
+
 
 
 
