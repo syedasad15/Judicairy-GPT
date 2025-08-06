f0fd9c29cba0f89a714c307ce4f15dc2bd7539d8
syedasad15 <144409006+syedasad15@users.noreply.github.com>
2025-08-06 11:23:13 +0500

    Update main.py

--- Implementation/main.py
+++ Implementation/main.py
@@ -458,10 +458,10 @@ if submitted and (user_input or st.session_state.uploaded_case_text) and file_va
         "message": response
     })
 
-    if st.session_state.chat_titles[chat_id] == "New Case":
-        title = generate_chat_title(query)
-        st.session_state.chat_titles[chat_id] = title if title else "Untitled Case"
+    title = generate_chat_title(query)
+    st.session_state.chat_titles[chat_id] = title if title else "Untitled Case"
     st.rerun()
+
 
 
 
